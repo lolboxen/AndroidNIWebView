ff0a8d31ce6717d11132b3d129f61660327828e6
Trent Ahrens <trent.ahrens@gmail.com>
2012-11-20 14:46:24 -0500

    First commit

---
+++ NIWebView.java
@@ -0,0 +1,103 @@
+/**
+ * Created by Trent Ahrens on 11/20/2012
+ *
+ * NIWebView allows you to evaluate and get result of javascript from native code.
+ */
+
+package com.lolboxen.ni;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.webkit.WebView;
+
+public class NIWebView extends WebView {
+
+	public static String NI_LOG_TAG = "com.lolboxen.NI";
+	
+	private NIWebViewSyncInterface syncInterface;
+
+	public NIWebView(Context context) {
+		super(context);
+		setup();
+	}
+
+	public NIWebView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setup();
+	}
+
+	public NIWebView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		setup();
+	}
+	
+	private void setup()
+	{
+		getSettings().setJavaScriptEnabled(true);
+		
+		syncInterface = new NIWebViewSyncInterface();
+		addJavascriptInterface(syncInterface, "NISyncInterface");
+	}
+	
+	public String stringByEvaluatingJavaScript(String javascript)
+	{
+		String escapedJavascript = javascript.replaceAll("\\\\", "\\\\\\\\").replaceAll("\\\"", "\\\\\"");
+		String finalCode = 
+				"javascript:try { " +
+					"NISyncInterface.didCompile();" +
+					"NISyncInterface.setReturnValue(\"" + escapedJavascript + "\");" +
+				"} catch(err) {" +
+					"NISyncInterface.setReturnValue('');" +
+				"}";
+		Log.v(NI_LOG_TAG, finalCode);
+		this.loadUrl(finalCode);
+		return syncInterface.getReturnValue();
+	}
+	
+	private class NIWebViewSyncInterface
+	{
+		private CountDownLatch compileLatch;
+		private CountDownLatch finishLatch;
+		private String returnValue;
+		
+		public String getReturnValue()
+		{
+			returnValue = "";
+			
+			compileLatch = new CountDownLatch(1);
+			finishLatch = new CountDownLatch(1);
+			
+			try {
+				compileLatch.await(1, TimeUnit.SECONDS);
+			} catch (InterruptedException e) {
+				Log.e(NI_LOG_TAG, "script did not compile");
+				return returnValue;
+			}
+			
+			try {
+				finishLatch.await(30, TimeUnit.SECONDS);
+				return returnValue;
+			} catch (InterruptedException e) {
+				Log.e(NI_LOG_TAG, "Timed out waiting for JS response");
+				throw new RuntimeException("Timed out waiting for JS response");
+			}
+		}
+		
+		@SuppressWarnings("unused")
+		public void setReturnValue(String returnValue)
+		{
+			this.returnValue = returnValue;
+			finishLatch.countDown();
+		}
+		
+		@SuppressWarnings("unused")
+		public void didCompile()
+		{
+			compileLatch.countDown();
+		}
+	}
+}
